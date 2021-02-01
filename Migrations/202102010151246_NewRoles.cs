6f603c1c0ab439120619e4f75320c229c1a5e3c4
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2021-01-31 21:15:51 -0500

    adding more user roles
    Added admin role

---
+++ Migrations/202102010151246_NewRoles.cs
@@ -0,0 +1,16 @@
+﻿namespace BugTrack.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewRoles : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
